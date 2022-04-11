816c9ebf002164f0615f38563f2176f3009059f1
Karol Zedzian <kzedzian@objectivity.co.uk>
2022-04-11 13:22:14 +0200

    delete solutions

--- Tasks/NHibernateTests.cs
+++ Tasks/NHibernateTests.cs
@@ -16,12 +16,9 @@ namespace NHibernateUpskill.Tests
         [Fact]
         public void ShouldFilterCarByType()
         {
-            var result = Session.QueryOver<Car>()
-                .Where(x => x.Type == (int)CarTypeKeys.Honda)
-                .List<Car>();
 
-            result.Should().HaveCount(1);
-            result[0].PlateNumber.Should().Be(HondaPlate);
+            //result.Should().HaveCount(1);
+            //result[0].PlateNumber.Should().Be(HondaPlate);
         }
 
         [Fact]
@@ -35,14 +32,9 @@ namespace NHibernateUpskill.Tests
         public void ShouldFilterCarByMileage()
         {
             const int kilometersLessThan = 250;
-            var result = Session.QueryOver<Car>()
-                .JoinQueryOver(x => x.Mileage)
-                .Where(x => (x.Value < kilometersLessThan && x.UnitOfMeasureId == (int)UnitOfMeasureKeys.Kilometers) ||
-                            (x.Value < kilometersLessThan / KmToMile && x.UnitOfMeasureId == (int)UnitOfMeasureKeys.Miles))
-                .List<Car>();
 
-            result.Should().HaveCount(1);
-            result[0].PlateNumber.Should().Be(HondaPlate);
+            //result.Should().HaveCount(1);
+            //result[0].PlateNumber.Should().Be(HondaPlate);
         }
 
         [Fact]
@@ -50,11 +42,6 @@ namespace NHibernateUpskill.Tests
         {
             var kilometersLessThan = 250;
             var query = $@"
-select _car.PlateNumber
-from Car _car
-    inner join _car.Mileage _mileage
-        with _mileage.Value < {kilometersLessThan}
-group by _car.PlateNumber
 ";
             var result = Session.CreateQuery(query).List<string>();
             result.Should().HaveCount(1);
@@ -68,17 +55,8 @@ group by _car.PlateNumber
             var journeyStartedAfter = new DateTime(2021, 12, 11);
             var anySegmentLengthGreaterThan = 10_000;
 
-            var result = Session.QueryOver<Car>()
-                .Where(x => x.Type == (int)CarTypeKeys.Honda)
-                .JoinQueryOver<Journey>(x => x.Journeys)
-                .Where(x => x.StartedAt > journeyStartedAfter)
-                .JoinQueryOver<Segment>(x => x.Segments)
-                .Where(x => x.LengthMeters > anySegmentLengthGreaterThan)
-                .TransformUsing(Transformers.DistinctRootEntity)
-                .List<Car>();
-
-            result.Should().HaveCount(1);
-            result[0].PlateNumber.Should().Be(HondaPlate);
+            //result.Should().HaveCount(1);
+            //result[0].PlateNumber.Should().Be(HondaPlate);
         }
 
         [Fact]
@@ -92,7 +70,7 @@ group by _car.PlateNumber
         }
 
         [Fact]
-        public void ShouldFilterBySegmentsAndFeatures()
+        public void ShouldFilterBySegmentsAndFeatures_UsingAliases()
         {
             var hasFeature = (int) FeatureKeys.SeatBelts;
             var anySegmentLengthGreaterThan = 150_000;
@@ -102,17 +80,8 @@ group by _car.PlateNumber
             Journey journeyAlias = null;
             Segment segmentAlias = null;
 
-            var result = Session.QueryOver(() => carAlias)
-                .JoinAlias(() => carAlias.Journeys, () => journeyAlias)
-                .JoinAlias(() => journeyAlias.Segments, () => segmentAlias)
-                .JoinAlias(() => carAlias.Features, () => featureAlias)
-                .Where(x => featureAlias.Id == hasFeature)
-                .Where(x => segmentAlias.LengthMeters > anySegmentLengthGreaterThan)
-                .TransformUsing(Transformers.DistinctRootEntity)
-                .List<Car>();
-
-            result.Should().HaveCount(1);
-            result[0].PlateNumber.Should().Be(BmwPlate);
+            //result.Should().HaveCount(1);
+            //result[0].PlateNumber.Should().Be(BmwPlate);
         }
 
         [Fact]
@@ -129,11 +98,6 @@ group by _car.PlateNumber
         public void HQL_ShouldSelectCarsWithMoreThanOneJourney()
         {
             var query = $@"
-select car.PlateNumber
-from Car as car
-    left join car.Journeys as Journeys
-group by car.PlateNumber
-having count(Journeys) > 1
 ";
             var result = Session.CreateQuery(query).List<string>();
 
@@ -146,13 +110,6 @@ having count(Journeys) > 1
         {
             var totalSegmentsLengthInCityIsGreaterThan = 150_000;
             var query = $@"
-select _car.PlateNumber
-from Car _car
-    left join _car.Journeys _journeys
-    left join _journeys.Segments _segments
-        with _segments.IsCity = false
-group by _car.PlateNumber
-having sum(_segments.LengthMeters) > {totalSegmentsLengthInCityIsGreaterThan}
 ";
 
             var result = Session.CreateQuery(query).List<string>();
